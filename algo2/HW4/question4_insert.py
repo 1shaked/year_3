f4356b23333e283089da94f3f73d7d28a321972b
1shaked <40497915+1shaked@users.noreply.github.com>
2024-10-09 03:35:42 +0300

    feat: Implement insert and delete functions for skip list, add findOrderStatistic method

--- algo2/HW4/question4_insert.py
+++ algo2/HW4/question4_insert.py
@@ -1,17 +1,42 @@
 
+import random
+
+
 class Node:
     pass
 # handle insert operation in a skip list, where we have len of the distance between the nodes in level 0
 def insert(t, v):
     prevEl , nextEl = search(t, v)
     prevParentPath = searchPath(t, prevEl) 
-    # tempPath = prevParentPath
+    prevEl.next = Node(v, prevEl.next)
+    v.next = nextEl
     for path in prevParentPath:
         if path.dir != 'UP':
             continue
         if path.v <= v and path.next.v >= v:
             path.size += 1
         
+
+
+def insert(t, v, level):
+    prevEl , nextEl = search(t, v)
+    prevParentPath, distanceFromV = searchPath(t, prevEl) 
+    distanceToNext = prevParentPath.size
+    remainingDistance = distanceToNext - distanceFromV
+    prevEl.next = Node(v, prevEl.next)
+    v.next = nextEl
+    for path in prevParentPath:
+        if path.dir != 'UP':
+            continue
+        if path.v <= v and path.next.v >= v:
+            path.size += 1
+    ADD_TO_TOP = random.choice([True, False])
+    if ADD_TO_TOP:
+        insert(t.levels[level + 1])
+        
+    return t
+        
+
 
 
 def search(t, v):
@@ -22,3 +47,28 @@ def searchPath(t, v):
 
 def split(t):
     pass
+
+def delete(t, v):
+    prevEl , nextEl, didFound = search(t, v)
+    prevParentPath, distanceFromV = searchPath(t, prevEl) 
+    if not didFound:
+        prevEl.size - 1
+    else: 
+        prevEl.size += v.size - 1
+
+    prevEl.next = nextEl
+    delete(t.levels[1] , v  )
+
+
+
+def findOrderStatistic(t, k, init):
+    start = init
+    end = init.next
+    if start.size == k:
+        return start
+    if start.size > k and end.size < k:
+        return findOrderStatistic(t, k , start)
+    if start.size < k:
+        return findOrderStatistic(t, k - start.size, end)
+    
+    
