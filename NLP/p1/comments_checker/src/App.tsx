bfc1e6b73273552d1896c36b547f01ee1bc5b29f
1shaked <shaked1hen@gmail.com>
2024-12-30 18:20:41 +0200

    builkd

--- NLP/p1/comments_checker/src/App.tsx
+++ NLP/p1/comments_checker/src/App.tsx
@@ -1,34 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <QueryClientProvider client={queryClient}>
+        <div>
+
+          <CommentsChecker />
+        </div>
+      </QueryClientProvider>
+
     </div>
-  )
+  );
 }
 
-export default App
+export function CommentsChecker() {
+  const [comments, setComments] = useState<string[]>([
+    "This is an amazing movie",
+  ]);
+  const [reviews , setReviews] = useState<{
+    probability: number;
+    label: 'negative' | 'positive';
+  }[]>([]);
+  const [modelType, setModelType] = useState<'GRU-TWO-LAYERS' | 'LSTM-TWO-LAYERS'>('GRU-TWO-LAYERS');
+  
+  return (
+    <div>
+      <div style={{ display: "flex", gap: '2rem'}}>
+        
+        
+        <div>
+          <label>
+            <input
+              type="radio"
+              value="GRU-TWO-LAYERS"
+              checked={modelType === 'GRU-TWO-LAYERS'}
+              onChange={() => setModelType('GRU-TWO-LAYERS')}
+            />
+            GRU-TWO-LAYERS
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="radio"
+              value="LSTM-TWO-LAYERS"
+              checked={modelType === 'LSTM-TWO-LAYERS'}
+              onChange={() => setModelType('LSTM-TWO-LAYERS')}
+            />
+            LSTM-TWO-LAYERS
+          </label>
+        </div>
+      </div>
+      <div>
+        <button
+        onClick={() => {
+          setComments([...comments, "Movie is amazing"]);
+        }}
+        >Add comment</button>
+
+      </div>
+      {comments.map((comment, i) => (
+        <div key={i}>
+          <input
+            style={{ border: "1px solid green" }}
+            type="text"
+            value={comment}
+            onChange={(e) => {
+              const newComments = comments.slice();
+              newComments[i] = e.target.value;
+              setComments(newComments);
+            }}
+          />
+          <button
+            onClick={() => {
+              const newComments = comments.slice();
+              newComments.splice(i, 1);
+              setComments(newComments);
+              // 
+              setReviews(reviews.filter((_, index) => index !== i));
+            }}
+          >
+            Delete
+          </button>
+          <div>
+            {reviews?.at(i)?.label} {reviews?.at(i)?.probability}
+          </div>
+        </div>
+      ))}
+      <div>
+        <button
+        onClick={async () => {
+          const response = await fetch(`https://nlp-comments-analyser.onrender.com/predict/`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ comments, model: modelType }),
+          });
+          const data = await response.json();
+          console.log(data);
+          setReviews(data.results);
+        }}
+        >Send to check</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
