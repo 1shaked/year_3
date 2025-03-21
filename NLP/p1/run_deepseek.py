d3dc98dd2a97bd3bd0a650da34b7a0f87a53f990
1shaked <shaked1hen@gmail.com>
2025-03-21 17:12:03 +0200

    add run_deepseek.py for querying and testing the DeepSeek model; implement sentiment analysis on movie reviews

---
+++ NLP/p1/run_deepseek.py
@@ -0,0 +1,32 @@
+import ollama
+import json as js
+from utils_classes import load_and_process_comments
+def query_deepseek(prompt):
+    # response = ollama.chat(model='deepseek-r1:1.5b', messages=[{'role': 'user', 'content': prompt}])
+    response = ollama.chat(model='llama3.2', messages=[{'role': 'user', 'content': prompt}])
+    return response['message']['content']
+
+
+def test_deepseek_model():
+    _, _, test_comments, test_labels = load_and_process_comments(
+        train_path='train',
+        batch_size=50,
+    )
+    answers = []
+    for index, batch in enumerate(test_comments):
+        comments ,labels = batch
+        for inner_index, comment in enumerate(comments):
+            answer = query_deepseek(f'''for the following movie review, tell in (0=negative, 1=positive) sentiment:
+                {comment}
+
+                answer only with the number (0 or 1) no more text whatsoever''')
+            actual_answer = str(labels[inner_index])
+            answers.append([answer, actual_answer])
+            # save the answer
+            with open('deepseek_answers.json', 'w') as f:
+                f.write(js.dumps(answers))
+        print(f"Batch {index} done")
+    print(f"Accuracy: {sum(answers) / len(answers)}")
+
+
+test_deepseek_model()
