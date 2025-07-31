283d1a790fd4d6326a53eac7970b51493267b0ae
1shaked <shaked1hen@gmail.com>
2025-07-31 21:40:49 +0300

    feat: enhance processing time sampling and order handling in entities.py 🛠️

--- simulation/project/entities.py
+++ simulation/project/entities.py
@@ -22,8 +22,10 @@ class ProductType:
         """Sample processing time for a given station."""
         lambda_value = self.processing_time_distributions.get(station_id)
         if lambda_value:
-            val = np.random.exponential(scale=lambda_value, size=1)
-            return val[0]
+            val = np.random.exponential(scale=lambda_value, size=1)[0]
+            while val > WORKING_DAY_LENGTH or val <= MIN_PROCESSING_TIME:  # Ensure the processing time does not exceed the working day length
+                val = round(np.random.exponential(scale=lambda_value, size=1)[0], ROUND_DECIMAL_PLACES)
+            return round(val, ROUND_DECIMAL_PLACES)
         raise ValueError(f"No distribution for station {station_id}")
 
 class Supplier:
@@ -69,7 +71,7 @@ class ProductInstance:
     """
     def __init__(self, product_type: ProductType, order_id: int | None, status: str = INGREDIENTS_WAITING, amount: int = 1 , product_designation: str | None = None):
         '''
-        The status can be (INGREDIENTS_WAITING, INGREDIENTS_ORDERED, FULFILLED)
+        The status can be (INGREDIENTS_WAITING, INGREDIENTS_ORDERED, )
         '''
         self.product_type = product_type
         self.order_id = order_id
@@ -119,17 +121,51 @@ class Station:
         return None
     
     def start_processing(self, index = 0) -> float:
-        self.queue[index][0].status = STATUS_PROCESSING_MACHINE
+        # check if a list first
+        if isinstance(self.queue[index][0], list):
+            for item in self.queue[index][0]:
+                item.status = STATUS_PROCESSING_MACHINE
+        else:
+            self.queue[index][0].status = STATUS_PROCESSING_MACHINE
         self.working_item_index = index
         return self.queue[index][1]
     
-    def decrement_processing_time(self, index: int, time: float) -> Tuple[ProductInstance, float ] | Tuple[None, None]:
+    def decrement_processing_time(self, index: int, time: float, ) -> Tuple[ProductInstance, float ] | Tuple[None, None]:
         """Decrement the processing time of the product instance at the given index."""
         if 0 <= index < len(self.queue):
             product_instance, processing_time = self.queue[index]
             new_processing_time = max(0, processing_time - time)
             self.queue[index] = (product_instance, new_processing_time)
             if new_processing_time == 0:
+                if self.station_id == STATION_THREE_ID:
+                    # this mean we are done preparing the product z create the product instance 
+                    product_designation = self.queue[index][0][0].product_designation
+                    if product_designation is None:
+                        raise ValueError("Product designation is None for product z.")
+                    
+                    # calculate the cost
+                    cost = 0.0
+                    for item in self.queue[index][0]:
+                        cost += item.product_type.cost
+                    product_type = ProductType(
+                            product_id=product_designation,
+                            processing_time_distributions={
+                                1: 0,  # Station 1
+                                2: 0,  # Station 2
+                                3: 0   # Station 3
+                            },
+                            volume_per_unit=PRODUCT_VOLUME[product_designation],
+                            cost=cost
+                        )
+                    product_instance = ProductInstance(
+                        product_type=product_type,
+                        order_id=self.queue[index][0][0].order_id,
+                        status=STATUS_COMPLETED_MACHINE,
+                        amount=self.queue[index][0][0].amount * FACTOR_DICT[product_designation],
+                        product_designation=None
+                    )
+                    self.queue.pop(0)
+                    return product_instance, new_processing_time
                 product_instance.status = STATUS_COMPLETED_MACHINE
                 # pop the first item from the queue
                 self.queue.pop(0)
@@ -139,6 +175,8 @@ class Station:
             return None , None
         return None , None
     
+    
+
     def decrement_processing_time_for_working_item(self, time: float) -> Tuple[ProductInstance | None, float]:
         """Decrement the processing time of the currently working item."""
         if self.working_item_index is not None:
@@ -210,14 +248,14 @@ class Station:
         # we need to find the index of the item which have the same order_id, and the same product_designation
         product_instance.product_designation
         product_instance.order_id
-        for index in range(self.queue):
+        for index in range(len(self.queue)):
             items_list = self.queue[index][0]
             if not isinstance(items_list , List) or len(items_list) == 0:
                 raise ValueError(f'Expected List but did not get it, or the len(item) is empty {len(item)}')
             
             item = None
             for product_item in items_list:
-                if product_item.product_id == PRODUCT_ID_Z:
+                if product_item.product_type.product_id == PRODUCT_ID_Z:
                     item = product_item
                     break
             if item is None:
@@ -235,7 +273,7 @@ class Order:
         self.order_id = order_id
         self.products = products
         self.due_time = due_time
-        self.status = INGREDIENTS_WAITING  # Order status can be 'WAITING', 'INGREDIENTS_ORDERED', 'FULFILLED', etc.
+        self.status = INGREDIENTS_WAITING  # Order status can be 'WAITING', 'INGREDIENTS_ORDERED', INGREDIENTS_PROCESSED ,'ORDER_FULFILLED', etc.
 
     def __str__(self):
         return f"Order ID: {self.order_id}, Products: {[(product.product_id, quantity) for product, quantity in self.products]}, Due Time: {self.due_time}, Status: {self.status}"
@@ -243,7 +281,7 @@ class Order:
         return f"Order({self.order_id}, {self.products}, {self.due_time}, {self.status})"
     def mark_fulfilled(self):
         """Mark the order as fulfilled."""
-        self.status = FULFILLED
+        self.status = ORDER_FULFILLED
 
     def calculate_order_cost(self) -> float:
         for product, q in self.products:
