929a92f475cf9c7f392e4f0fc1f8509f2f9d50b3
1shaked <40497915+1shaked@users.noreply.github.com>
2025-08-04 23:41:37 +0300

    fix: update OPTION iteration in sim.py to use the first element of GET_NEXT_ORDER_OPTIONS 🛠️

--- simulation/project/sim.py
+++ simulation/project/sim.py
@@ -8,8 +8,8 @@ from main import (
 
 if __name__ == "__main__":
     for i in range(1, 2):
-        for ordering_strategy in ORDERING_STRATEGIES:
-            for OPTION in [GET_NEXT_ORDER_OPTIONS[0]]:
+        for ordering_strategy in [ORDERING_STRATEGIES[0]]:
+            for OPTION in GET_NEXT_ORDER_OPTIONS:
                 for ALGORITHM in ALGORITHM_SORTING_OPTIONS:
                     sim = SimulationManager(OPTION, ALGORITHM, ordering_strategy)
                     sim.run()
